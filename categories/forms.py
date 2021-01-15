ad03894d720c4b2707e9ea8865dcb1dc834d4613
Jorge Parada Fernandez <jorge.parada@ctbarbanza.com>
2021-01-15 13:01:12 +0100

    .

--- categories/forms.py
+++ categories/forms.py
@@ -3,6 +3,42 @@ from .models import *
 
 
 class CategoryForm(forms.ModelForm):
+	name = forms.CharField()
 	class Meta:
 		model = Category
 		fields = ['name']
+
+		error_messages = {
+			'name': {
+				'required': ('Please let us know what to call you!'),
+				'max_length': ("This writer's name is too long."),
+			},
+		} 
+
+	def clean_name(self):
+		data = self.cleaned_data['name']
+		print("DATA"+str(data))
+		if None is data:
+			return None 
+		return data
+
+class NameForm(forms.Form):
+	name = forms.CharField(label='Your name', max_length=100)
+
+	def clean_name(self):
+		data = self.cleaned_data['name']
+		if None is data:
+			return None 
+		if data.strip():
+			return data.strip()
+		return None # ''
+
+
+class IDForm(forms.Form):
+	id = forms.IntegerField()
+
+	def clean_id(self):
+		data = self.cleaned_data['id']
+		if None is data:
+			return None  
+		return data
--- categories/views.py
+++ categories/views.py
@@ -1,30 +1,53 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from django.views.decorators.csrf import csrf_exempt
 from .forms import *
 import ujson
 
 # Create your views here.
+@csrf_exempt
 def create(request): 
 	# Crear una Categoria
-	items = []
-
-	form = CategoryForm(request.POST)
+	form = NameForm(data=request.POST) 
+	if (form.is_valid()):
+		cat = Category()
+		cat.name = form.cleaned_data['name']
+		cat.save() 
+		return HttpResponse(cat)
+	else:
+		res = form.errors 
+		return HttpResponse(res) 
 	
 
-	return HttpResponse(items)
-
 def read(request): 
-	# Listar todas las categorias
+	# Listar todas las categorias 
 	items = Category.objects.all()
-	return HttpResponse(items) 
+	ar = []
+	for item in items:
+		ar.append( str(item) )
+	return HttpResponse(ar)
 
+@csrf_exempt
 def update(request): 
 	# Editar una Categoria
 	items = []
 	return HttpResponse(items)
 
+@csrf_exempt
 def delete(request): 
-	# Borrar una Categoria
-	items = []
-	return HttpResponse(items)
+	# Borrar una Categoria 
+	form = IDForm(data=request.POST) 
+	if (form.is_valid()):
+		id = form.cleaned_data['id']
+		cat = Category.objects.filter(id=id)
+		if None is cat:
+			return HttpResponse("ID No Encontrado")
+		else:
+			strCat = str(cat)
+			cat.delete()  
+			return HttpResponse(strCat)
+	else:
+		res = form.errors 
+		return HttpResponse(res) 
+		
